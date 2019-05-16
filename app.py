286d182cde32dc4c35adc91fcdba9609946768b0
pinchiachen <47185853+pinchiachen@users.noreply.github.com>
2019-05-16 17:02:20 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+# Channel Secret
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    movie_name = event.message.text
+    
+    #抓取前 10 頁所有文章標題
+    title = []
+    page_count = 10
+    for i in range(page_count):   
+        res = requests.get('https://www.ptt.cc/bbs/movie/search?page=%d&q=%s'%(i+1, movie_name))
+        soup = BeautifulSoup(res.text)
+        for entry in soup.select('.r-ent'):
+            title.append(entry.select('.title')[0].text)
+
+    #找出標題有'雷'且有']'且沒有'Re'之分類，刪除多於之空格
+    title_index = []
+    for i in range(len(title)-1, -1, -1):
+        if '雷' in title[i] and ']' in title[i] and 'Re' not in title[i]:
+            title_index.append(title[i].split(']', 1)[0].split('[', 1)[1].replace(' ',''))
+
+    #計算評價
+    good_count = 0
+    ordinary_count = 0
+    bad_count = 0
+    total_count = 0
+    for evaluation in title_index:
+        if '好' in evaluation:
+            good_count += 1
+        elif '爛' in evaluation or '負' in evaluation:
+            bad_count += 1
+        elif '普' in evaluation and '好'  not in evaluation and '爛'  not in evaluation and '負'  not in evaluation:
+            ordinary_count += 1
+
+    #印出結果
+    total_count = good_count + ordinary_count + bad_count
+    if total_count == 0:
+        response = '查無資料'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '查無資料'))
+    else:
+        good_percent = good_count / total_count * 100
+        ordinary_percent = ordinary_count / total_count * 100
+        bad_percent = bad_count / total_count * 100
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '評價總共有 %d 篇\n好雷有 %d 篇 / 好雷率為 %.2f %%\n普雷有 %d 篇 / 普雷率為 %.2f %%\n負雷有 %d 篇 / 負雷率為 %.2f %%'%(total_count, good_count, good_percent, ordinary_count, ordinary_percent, bad_count, bad_percent)))
+
+   
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
